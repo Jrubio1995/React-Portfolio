9a545d6ee812be7a264dd5aa69e5747318d358da
JesusRubio <jesusarizarubio@gmail.com>
2020-12-01 18:45:34 -0600

    Updates

---
+++ precache-manifest.d0d886994da1f7cb2a5ecdf777bf0a5d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "790a6bd19e39bf2face92da0db6f5da3",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "c8052172d5b01e307e79",
+    "url": "/React-Portfolio/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "2f5f2bdc6bbdb50c57bf",
+    "url": "/React-Portfolio/static/css/main.77831712.chunk.css"
+  },
+  {
+    "revision": "c8052172d5b01e307e79",
+    "url": "/React-Portfolio/static/js/2.1b9f5213.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/React-Portfolio/static/js/2.1b9f5213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f5f2bdc6bbdb50c57bf",
+    "url": "/React-Portfolio/static/js/main.cb2ea80e.chunk.js"
+  },
+  {
+    "revision": "7c35ff808cd94c4ce20c",
+    "url": "/React-Portfolio/static/js/runtime-main.0ad08de7.js"
+  },
+  {
+    "revision": "8f9a337bc23c8f3102218d327fa583bd",
+    "url": "/React-Portfolio/static/media/Gamescreen.8f9a337b.png"
+  },
+  {
+    "revision": "40bc1b6e28d345c84cee4165c25f9456",
+    "url": "/React-Portfolio/static/media/Screenshot (1).40bc1b6e.png"
+  },
+  {
+    "revision": "8c40e2addc12e600804c29245fd6ce94",
+    "url": "/React-Portfolio/static/media/Screenshot (2).8c40e2ad.png"
+  },
+  {
+    "revision": "afc36746f79d1d32d90a3fd35482b298",
+    "url": "/React-Portfolio/static/media/Screenshot (3).afc36746.png"
+  },
+  {
+    "revision": "1decce68fb3808e1b590512aed2e41a5",
+    "url": "/React-Portfolio/static/media/Screenshot (31).1decce68.png"
+  }
+]);
